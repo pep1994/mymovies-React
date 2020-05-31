4d6ff894553f1b4ef780ac94121b3590c9ebdf3e
Marco Petrini <marcopetrini1994@gmail.com>
2020-05-31 18:55:20 +0200

    gestione stato componente navbar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function SearchBar() {
-    return (
-        <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    )
+export default class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            searchVal: ""
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({
+            searchVal: e.target.value
+        })
+    }
+
+
+    render () {
+
+        return (
+            <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" value={this.state.searchVal} placeholder="Search" aria-label="Search" onChange={this.changeHandler}/>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        )
+    }
 }
 
 
